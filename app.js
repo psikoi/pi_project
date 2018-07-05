aeda4a1773bd0105b896da26e6457889fab597ce
tiagofsantos <tiago.afsantos@hotmail.com>
2018-07-05 21:16:20 +0100

    Completei endpoints

--- app.js
+++ app.js
@@ -10,13 +10,24 @@ app.use(express.static("www"));
 
 // GET
 app.get("/player", requestHandlers.getPlayers);
+app.get("/player/today", requestHandlers.getPlayersToday);
+app.get("/player/week", requestHandlers.getPlayersWeek);
+app.get("/player/month", requestHandlers.getPlayersMonth);
 app.get("/session", requestHandlers.getSessions);
+app.get("/session/today", requestHandlers.getSessionsToday);
+app.get("/session/week", requestHandlers.getSessionsWeek);
+app.get("/session/month", requestHandlers.getSessionsMonth);
 app.get("/statistic", requestHandlers.getStatistics);
 
 // POST
 app.post("/player", requestHandlers.addUpdatePlayer);
 app.post("/session", requestHandlers.addUpdateSession);
 app.post("/statistic", requestHandlers.addUpdateStatistic);
+
+// PUT
+app.put("/player", requestHandlers.addUpdatePlayer);
+app.put("/session", requestHandlers.addUpdateSession);
+app.put("/statistic", requestHandlers.addUpdateStatistic);
 
 // DELETE
 app.delete("/player", requestHandlers.deletePlayer);
